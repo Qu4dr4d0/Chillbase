366c6c3382def9adfa906fec5d67f7fe60dac50a
João Vitor Quadrado <110358936+Qu4dr4d0@users.noreply.github.com>
2022-10-04 19:44:05 -0300

    Javascript added
    - Audios added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,64 @@
+const audioLightRain = new Audio('./assets/audios/light-rain.mp3')
+const audioHeavyRain = new Audio('./assets/audios/heavy-rain.mp3')
+const audioWinter = new Audio('./assets/audios/winter-wind.mp3')
+const audioJungle = new Audio('./assets/audios/deep-jungle.mp3')
+const audioForest = new Audio('./assets/audios/forest.mp3')
+const audioOcean = new Audio('./assets/audios/ocean.mp3')
+const audioCity = new Audio('./assets/audios/city.mp3')
+const audioBusyPlace = new Audio('./assets/audios/busy-place.mp3')
+
+function stopAll() {
+  audioLightRain.pause()
+  audioHeavyRain.pause()
+  audioWinter.pause()
+  audioJungle.pause()
+  audioForest.pause()
+  audioOcean.pause()
+  audioCity.pause()
+  audioBusyPlace.pause()
+}
+
+function lightRain() {
+  stopAll()
+  audioLightRain.play()
+}
+
+function heavyRain() {
+  stopAll()
+  audioHeavyRain.play()
+}
+
+function winter() {
+  stopAll()
+  audioWinter.play()
+}
+
+function jungle() {
+  stopAll()
+  audioJungle.play()
+}
+
+function forest() {
+  stopAll()
+  audioForest.play()
+}
+
+function ocean() {
+  stopAll()
+  audioOcean.play()
+}
+
+function city() {
+  stopAll()
+  audioCity.play()
+}
+
+function busyPlace() {
+  stopAll()
+  audioBusyPlace.play()
+}
+
+
 
 
 
